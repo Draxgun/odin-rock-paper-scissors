bd3c04e0c79d58b12b8913dfe44c0613664df396
Draxgun <j.alejandro.banuelos@outlook.com>
2022-07-25 11:22:38 -0600

    Main game finished

--- javascript.js
+++ javascript.js
@@ -71,16 +71,19 @@ function getplayerChoice(){
 function playRound(playerSelection,computerSelection){
     let gameStatus = 0;
     if (playerSelection==computerSelection){
-        console.log("It's a tie!");
-        return gameStatus=0;
+        let gameText  = "It's a tie!";
+        gameStatus =0;
+        return [gameStatus,gameText];
     } 
     else if( playerSelection == "Rock" && computerSelection == "Scissor" || playerSelection == "Paper" && computerSelection == "Rock" || playerSelection == "Scissor" && computerSelection == "Paper"){
-            console.log("You win " + playerSelection + " beats " + computerSelection + "!");
-            return gameStatus=1;
+            let gameText= "You win " + playerSelection + " beats " + computerSelection + "!";
+            gameStatus = 1
+            return [gameStatus,gameText];;
         }
     else if ( computerSelection == "Rock" && playerSelection == "Scissor" || computerSelection == "Paper" && playerSelection == "Rock" || computerSelection == "Scissor" && playerSelection == "Paper") {
-            console.log("You loose " + computerSelection + " beats " + playerSelection + "!");
-            return gameStatus=-1;
+            let gameText = "You loose " + computerSelection + " beats " + playerSelection + "!";
+            gameStatus=-1;
+            return [gameStatus,gameText]
         }
         
 }
@@ -143,10 +146,11 @@ function game(){
     }
 }
 
-/* Game declaration */
-/* game() */
 
-
+/* Initial Value */
+let round = 0;
+let playerScore = 0;
+let cpuScore  = 0;
 
 
 /* Deletes element */
@@ -201,15 +205,20 @@ gameContent.appendChild(statsHUD);
 /* Round */
 const roundHUD = document.createElement('div');
 roundHUD.classList.add("roundHUD");
-roundHUD.textContent = "Round 0";
+roundHUD.textContent = `Round ${round}`;
 statsHUD.appendChild(roundHUD);
-
 
 /* Score */
 const scoreHUD = document.createElement('div');
 scoreHUD.classList.add("scoreHUD");
-scoreHUD.textContent = "Human: 0  CPU: 0";
+scoreHUD.textContent = `Human: ${playerScore}  CPU: ${cpuScore}`;
 statsHUD.appendChild(scoreHUD);
+
+/* Choose your weapon text! */
+const textChoice = document.createElement("div");
+textChoice.classList.add("textChoice");
+textChoice.textContent = "Choose your weapon!";
+gameContent.appendChild(textChoice);
 
 /* Choices */
 const choiceHUD = document.createElement('div');
@@ -218,58 +227,155 @@ gameContent.appendChild(choiceHUD);
 
 /*choice*/
 
+/*Rock*/
 const rock = document.createElement('div');
 rock.classList.add("choiceItem");
 choiceHUD.appendChild(rock);
 
-
-
-/*Rock*/
 const rockImage = document.createElement('img');
 rockImage.classList.add("imageChoice")
-rockImage.src = "./images/home.png";
+rockImage.src = "./images/rock.png";
 rock.appendChild(rockImage);
 
 const rockText = document.createElement('div');
-rockText.classList.add("rockText");
+rockText.classList.add("textChoice");
 rockText.textContent = "ROCK!";
 rock.appendChild(rockText)
 
-
 /*Paper*/
-const paper = document.createElement('img');
-paper.classList.add("imageChoice")
-paper.src = "./images/paper.png";
+
+const paper = document.createElement('div');
+paper.classList.add("choiceItem");
 choiceHUD.appendChild(paper);
 
+const paperImage = document.createElement('img');
+paperImage.classList.add("imageChoice")
+paperImage.src = "./images/paper.png";
+paper.appendChild(paperImage);
+
+const paperText = document.createElement('div');
+paperText.classList.add("textChoice");
+paperText.textContent = "PAPER!";
+paper.appendChild(paperText)
+
+
 /*Scissor*/
-const scissor = document.createElement('img');
-scissor.classList.add("imageChoice")
-scissor.src = "./images/home.png";
+const scissor = document.createElement('div');
+scissor.classList.add("choiceItem");
 choiceHUD.appendChild(scissor);
 
+const scissorImage = document.createElement('img');
+scissorImage.classList.add("imageChoice")
+scissorImage.src = "./images/scissor.png";
+scissor.appendChild(scissorImage);
+
+const scissorText = document.createElement('div');
+scissorText.classList.add("textChoice");
+scissorText.textContent = "SCISSOR!";
+scissor.appendChild(scissorText)
 
 
-menuButton.addEventListener ("click", () => {
-    delElement("gameContent");
-    restoreElement(deletedElement);
+/* Robot choice */
+const robotChoice = document.createElement('div');
+robotChoice.classList.add("robotChoice");
+gameContent.append(robotChoice)
 
-}); 
+/* Robot Head */
+const robotHead = document.createElement('img');
+robotHead .classList.add("imageChoice")
+robotHead .src = "./images/robotHead.png";
+robotChoice.appendChild(robotHead);
 
+/* Robot Text */
+const robotText = document.createElement('div');
+robotText.classList.add("textChoice");
+robotText.setAttribute('id','robotText');
+robotChoice.append(robotText);
+
+
+/*Game output */
+const winnerText = document.createElement("div");
+winnerText.classList.add("textChoice");
+winnerText.textContent = "";
+winnerText.setAttribute('id','winnerText');
+gameContent.append(winnerText);
 
 
 let addGame = () =>{
     mainContainer.insertBefore(gameContent,footer);
 }
 
-
-
-
-
+/* StartGAME */
 start.addEventListener("click", () => {
     deletedElement = delElement("mainContent");
     addGame();    
 }
 );
 
+/* Return to menu button */
+menuButton.addEventListener ("click", () => {
+    delElement("gameContent");
+    restoreElement(deletedElement);
+    resetScore();
+}); 
+
+/*Reset*/
+menuReset.addEventListener("click",() => {
+    resetScore();
+});
+
+/* Gameplay */
+rock.addEventListener("click", () =>{
+    let playerChoice = "Rock";
+    playGame(playerChoice);
+
+});
+
+paper.addEventListener("click",() =>{
+    let playerChoice = "Paper";
+    playGame(playerChoice);
+});
+
+scissor.addEventListener("click",() => {
+    let playerChoice = "Scissor";
+    playGame(playerChoice)
+});
+
+/* Function that plays game */
+let playGame = (playerChoice)=>{
+    let computerChoice = getcomputerChoice();
+    console.log(playerChoice);
+    console.log(computerChoice);
+    let gameResult = playRound(playerChoice,computerChoice);
+    let gameStatus = gameResult[0];
+    let gameText   = gameResult[1];
+    robotText.textContent = `My choice is ${computerChoice}!`;
+    winnerText.textContent = gameText;
+    round++;
+    if (gameStatus==0){
+        playerScore = playerScore;
+        cpuScore = cpuScore;
+    } else if (gameStatus==1) {
+        playerScore++ ;
+    } else if (gameStatus==-1) {
+        cpuScore++ ;
+    }
+    updateScore();
+    
+};
+
+let resetScore = () => {
+    playerScore = 0;
+    cpuScore = 0;
+    round = 0
+    updateScore();
+}
+
+
+/* Function that updates score and round */
+let updateScore = () => {
+    scoreHUD.textContent = `Human: ${playerScore}  CPU: ${cpuScore}`;
+    roundHUD.textContent = `Round ${round}`;
+
+}
 
